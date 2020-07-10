60b84b930fbe81de2ed82a05f7d280060c0cc002
just-hjkwon <just.hjkwon@gmail.com>
2020-07-10 13:23:53 +0900

    [#1] Implement IBUG Dataset.

---
+++ datasets/ibug_dataset.py
@@ -0,0 +1,22 @@
+from pytorch_tools.dataset import ImageDataSet
+
+
+class IBUGDataset(ImageDataSet):
+    def __init__(self, base_directory: str):
+        super(IBUGDataset, self).__init__(base_directory, "jpg")
+
+    @staticmethod
+    def train_datum_filter(file_path: str):
+        return None
+
+    @staticmethod
+    def validation_datum_filter(file_path: str):
+        return file_path
+
+    @staticmethod
+    def extract_label(file_path: str):
+        return 0
+
+    @staticmethod
+    def is_valid_annotation(image_width, image_height, annotation):
+        return True
