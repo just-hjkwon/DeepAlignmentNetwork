b61b6368732527cf88f6297c4c90fbaeb795a2c1
just-hjkwon <just.hjkwon@gmail.com>
2020-07-10 13:22:15 +0900

    [#1] Implement LFPW Dataset.

---
+++ datasets/lfpw_dataset.py
@@ -0,0 +1,28 @@
+from pytorch_tools.dataset import ImageDataSet
+
+
+class LFPWDataset(ImageDataSet):
+    def __init__(self, base_directory: str):
+        super(LFPWDataset, self).__init__(base_directory, "png")
+
+    @staticmethod
+    def train_datum_filter(file_path: str):
+        if "trainset" in file_path:
+            return file_path
+        else:
+            return None
+
+    @staticmethod
+    def validation_datum_filter(file_path: str):
+        if "testset" in file_path:
+            return file_path
+        else:
+            return None
+
+    @staticmethod
+    def extract_label(file_path: str):
+        return 0
+
+    @staticmethod
+    def is_valid_annotation(image_width, image_height, annotation):
+        return True
