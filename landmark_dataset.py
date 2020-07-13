2ba17d5d2bed098c0ea7cf666ec315e76bf50cec
just-hjkwon <just.hjkwon@gmail.com>
2020-07-13 16:23:56 +0900

    [#2] Prepare input and target.

--- landmark_dataset.py
+++ landmark_dataset.py
@@ -1,14 +1,26 @@
 import random
 
 from torch.utils.data.dataset import Dataset
+from torchvision import transforms
 import torch
 
 import numpy as np
+import cv2
+
+from pytorch_tools.image.cropper import Cropper
 
 
 class LandmarkDataset(Dataset):
-    def __init__(self, datasets: list, is_train: bool, average_landmark=None):
+    def __init__(self, datasets: list, is_train: bool, average_landmark: np.ndarray = None):
         super(LandmarkDataset, self).__init__()
+
+        self.target_size = 112
+        self.target_face_box_size_range = (self.target_size * 0.35, self.target_size * 0.75)
+        self.target_average_face_box_size = 60
+
+        self.transforms = transforms.Compose([
+            transforms.ToTensor()
+        ])
 
         self.fixed_label = 0
 
@@ -34,8 +46,11 @@ class LandmarkDataset(Dataset):
 
         self.landmarks = None
         self.average_landmark = None
+        self.face_boxes = None
 
         self.prepare_landmark_cache(average_landmark)
+
+        self.val_cache = {}
 
     def prepare_index_map(self):
         self.index_map = []
@@ -67,8 +82,9 @@ class LandmarkDataset(Dataset):
                 for i in range(dataset.count(fixed_label)):
                     self.index_map.append((fixed_label, dataset_index, i))
 
-    def prepare_landmark_cache(self, average_landmark=None):
+    def prepare_landmark_cache(self, average_landmark: np.ndarray = None):
         landmarks = []
+        face_boxes = []
 
         if average_landmark is None:
             _average_landmark = np.zeros((68, 2))
@@ -80,8 +96,10 @@ class LandmarkDataset(Dataset):
             landmark = np.array(annotation)
             landmarks.append(landmark)
 
-            landmark_center = landmark.mean(axis=0)
+            face_box = landmark.min(axis=0).tolist() + landmark.max(axis=0).tolist()
+            face_boxes.append(face_box)
 
+            landmark_center = landmark.mean(axis=0)
             normalized_landmark = landmark - landmark_center
 
             if average_landmark is None:
@@ -91,26 +109,36 @@ class LandmarkDataset(Dataset):
             _average_landmark /= len(self)
 
         self.landmarks = landmarks
+        self.face_boxes = face_boxes
 
         if average_landmark is None:
-            self.average_landmark = _average_landmark
-        else:
-            self.average_landmark = average_landmark
+            average_landmark = _average_landmark
 
-    def __getitem__(self, index):
+        average_face_box = face_box = average_landmark.min(axis=0).tolist() + average_landmark.max(axis=0).tolist()
+        box_width = face_box[2] - face_box[0]
+        box_height = face_box[3] - face_box[1]
+        box_length = max(box_width, box_height)
+        average_landmark_scale = self.target_average_face_box_size / box_length
+
+        average_landmark *= average_landmark_scale
+        average_landmark += self.target_size / 2.0
+
+        self.average_landmark = average_landmark
+
+    def __getitem__(self, index: int):
         if self.is_train is True:
             label, dataset_index, data_index = self.index_map[index]
-            image, annotation = self.datasets[dataset_index].get_datum(label, data_index)
 
-            image, target = self.create_input_and_target(image, annotation, label, index)
+            image, _ = self.datasets[dataset_index].get_datum(label, data_index)
+            annotation = [self.face_boxes[index], self.landmarks[index]]
 
-            image = self.augment(None, image)
+            image, target = self.create_input_and_target(image, annotation)
+
             image = np.copy(image[:, :, ::-1])
+            target = target.flatten()
 
-            image = self.transform(image)
-            target = int(target)
-
-            return image, torch.LongTensor([target])
+            image = self.transforms(image)
+            target = torch.tensor(target)
         else:
             if index in list(self.val_cache.keys()):
                 image, target = self.val_cache[index]
@@ -124,50 +152,53 @@ class LandmarkDataset(Dataset):
 
                 image, target = self.create_input_and_target(image, annotation, random_seed=index)
 
-                image = self.augment(index, image, is_infer=True)
                 image = np.copy(image[:, :, ::-1])
+                target = target.flatten()
 
-                self.val_cache[index] = [image, target]
+                image = self.transforms(image)
+                target = torch.tensor(target)
 
-            image = self.transform(image)
-            target = int(target)
+        return image, target
 
-            return image, torch.LongTensor([target])
-
-    @staticmethod
-    def create_input_and_target(image, annotation, random_seed=None):
+    def create_input_and_target(self, image, annotation: list, random_seed: int = None):
         random.seed(random_seed)
 
-        if target == 0 and random.randint(0, 1) == 1:
-            do_sample_at_background = True
-        else:
-            do_sample_at_background = False
+        face_box = np.array(annotation[0])
+        landmark = np.array(annotation[1])
 
-        rotating_margin_factor = (math.sqrt(2.0) - 1.0) / 2.0
+        box_width = face_box[2] - face_box[0]
+        box_height = face_box[3] - face_box[1]
+        box_length = max(box_width, box_height)
 
-        if do_sample_at_background is False:
-            face_box = FDDataset.make_box_from_landmark(annotation['landmark'])
+        scale = random.uniform(self.target_face_box_size_range[0] / box_length,
+                               self.target_face_box_size_range[1] / box_length)
 
-            rotating_margin_factor = (math.sqrt(2.0) - 1.0) / 2.0
-            margin_need = face_box[2] * rotating_margin_factor
+        width, height = image.shape[1], image.shape[0]
+        resized_width = int(round(width * scale))
+        resized_height = int(round(height * scale))
+        width_scale = resized_width / width
+        height_scale = resized_height / height
 
-            x = int(round(face_box[0] - margin_need))
-            y = int(round(face_box[1] - margin_need))
-            width = int(round(face_box[2] + (margin_need * 2.0)))
-            height = int(round(face_box[3] + (margin_need * 2.0)))
-        else:
-            min_length = int(math.ceil(112.0 + (2.0 * (112.0 * rotating_margin_factor))))
-            image_width = image.shape[1]
-            image_height = image.shape[0]
+        image = cv2.resize(image, (resized_width, resized_height))
 
-            sample_length = random.randint(min_length, min(image_width, image_height) - 1)
+        landmark[:, 0] *= width_scale
+        landmark[:, 1] *= height_scale
+        face_box[0::2] *= width_scale
+        face_box[1::2] *= height_scale
+        box_center = [face_box[0::2].mean(), face_box[1::2].mean()]
 
-            x = random.randint(0, image_width - sample_length - 1)
-            y = random.randint(0, image_height - sample_length - 1)
-            width = sample_length
-            height = sample_length
+        box_center[0] += random.randint(-5, 5)
+        box_center[1] += random.randint(-5, 5)
 
-        image = Cropper.crop(image, x, y, width, height)
+        crop_x = int(round(box_center[0] - 56))
+        crop_y = int(round(box_center[1] - 56))
+
+        image = Cropper.crop(image, crop_x, crop_y, self.target_size, self.target_size)
+        landmark[:, 0] -= crop_x
+        landmark[:, 1] -= crop_y
+
+        target = landmark - self.average_landmark
+        target.flatten()
 
         return image, target
 
